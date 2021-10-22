50d604f4015575d62faebccbfa6807f8babb03c6
Dillon <dillon@astin.dev>
2021-10-21 21:01:41 -0400

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,62 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <nav className="navbar is-dark is-fixed-top" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <div className="navbar-item">
+          Dillon Astin
+        </div>
+
+        <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
         </a>
-      </header>
-    </div>
+      </div>
+
+      <div className="navbar-menu">
+        <div className="navbar-start">
+          <a className="navbar-item">
+            Home
+          </a>
+
+          <a className="navbar-item">
+            Resume
+          </a>
+
+          <div className="navbar-item has-dropdown is-hoverable">
+            <a className="navbar-link">
+              More
+            </a>
+
+            <div className="navbar-dropdown">
+              <a className="navbar-item">
+                About
+              </a>
+              <a className="navbar-item">
+                Portfolio
+              </a>
+              <a className="navbar-item">
+                Contact
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <a className="button is-primary">
+                GitHub
+              </a>
+              <a className="button is-light">
+                LinkedIn
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 }
 
