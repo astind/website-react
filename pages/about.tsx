633d29322f07684e89484d0d93744d9a24da3d42
dillon <astindillon@gmail.com>
2022-01-30 16:37:16 -0500

    size change

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,7 +4,7 @@ import { NextPage } from "next";
 const AboutMe: NextPage = () => {
   return (
     <div>
-      <div className="hero">
+      <div className="hero is-medium is-info">
         <div className="hero-body">
           <h1 className="title">Dillon Astin</h1>
           <p className="subtitle">Software Engineer</p>
